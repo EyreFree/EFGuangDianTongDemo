9e6caedc312f66ce04d1cbe9bee6f5bbda258418
EyreFree <EyreFree@163.com>
2016-03-25 17:17:20 +0800

    Init

---
+++ GuangDianTongDemo/GuangDianTongDemo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  GuangDianTongDemo-Bridging-Header.h
+//  GuangDianTongDemo
+//
+//  Created by EyreFree on 16/3/25.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+#ifndef GuangDianTongDemo_Bridging_Header_h
+#define GuangDianTongDemo_Bridging_Header_h
+
+//MD5
+#import <CommonCrypto/CommonDigest.h>
+
+#endif /* GuangDianTongDemo_Bridging_Header_h */
---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://git.coding.net/hging/Specs.git'
+
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'Alamofire', '3.0.0'
